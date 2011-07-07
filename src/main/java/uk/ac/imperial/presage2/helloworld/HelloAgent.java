ed8249935d7c80e81805e7c1bda6f76013b87cb7
davekyle <dws004+github@googlemail.com>
2011-07-07 18:21:35 +0100

    Added the AgentInfoMessage class, and updated HelloAgent to deal with them.
    Agents still send HelloMessages but now keep a track of which agents
    they've met before, and propagate their ID info (UUID, Name,
    NetworkAddress) amongst themselves.

--- src/main/java/uk/ac/imperial/presage2/helloworld/HelloAgent.java
+++ src/main/java/uk/ac/imperial/presage2/helloworld/HelloAgent.java
@@ -3,14 +3,20 @@
  */
 package uk.ac.imperial.presage2.helloworld;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
+
+import dws04.utils.presage2.AgentIDTriple;
 
 import uk.ac.imperial.presage2.core.environment.ActionHandlingException;
 import uk.ac.imperial.presage2.core.environment.ParticipantSharedState;
 import uk.ac.imperial.presage2.core.environment.UnavailableServiceException;
 import uk.ac.imperial.presage2.core.messaging.Input;
+import uk.ac.imperial.presage2.core.messaging.Performative;
+import uk.ac.imperial.presage2.core.network.NetworkAddress;
 import uk.ac.imperial.presage2.core.util.random.Random;
 import uk.ac.imperial.presage2.util.environment.CommunicationRangeService;
 import uk.ac.imperial.presage2.util.location.HasLocation;
@@ -22,12 +28,23 @@ import uk.ac.imperial.presage2.util.participant.HasCommunicationRange;
 import uk.ac.imperial.presage2.util.participant.HasPerceptionRange;
 
 /**
- * @author Sam Macbeth
+ * @author Sam Macbeth, dws04
  *
  */
 public class HelloAgent extends AbstractParticipant implements HasLocation, HasPerceptionRange, HasCommunicationRange {
 	
-	class State {
+	class DataStore {
+		LinkedHashMap<UUID,AgentIDTriple> knownAgents;
+		
+		/**
+		 * Let the agent keep a track of it's own "business card"
+		 * TODO make a listener or something for this ?
+		 * So that the agent knows when it should update other people if it changes.
+		 */
+		AgentIDTriple myAgentIDTriple;
+	}
+	
+	class EnvironmentState {
 		
 		Location loc;
 		
@@ -37,15 +54,20 @@ public class HelloAgent extends AbstractParticipant implements HasLocation, HasP
 		
 	}
 	
-	private State state = new State();
+	private DataStore dataStore = new DataStore();
+	
+	private EnvironmentState environmentState = new EnvironmentState();
 	
 	private ParticipantLocationService locationService;
 	
 	public HelloAgent(UUID id, String name, Location loc, double perceptionRange, double communicationRange) {
 		super(id, name);
-		state.loc = loc;
-		state.perceptionRange = perceptionRange;
-		state.communicationRange = communicationRange;
+		environmentState.loc = loc;
+		environmentState.perceptionRange = perceptionRange;
+		environmentState.communicationRange = communicationRange;
+		dataStore.knownAgents = new LinkedHashMap<UUID,AgentIDTriple>();
+		// Don't yet know our NetworkAddress, but can fill in the rest
+		dataStore.myAgentIDTriple = new AgentIDTriple(id, name, null);
 	}
 	
 	@Override
@@ -57,38 +79,42 @@ public class HelloAgent extends AbstractParticipant implements HasLocation, HasP
 			logger.warn(e);
 			this.locationService = null;
 		}
+		// Agent should now know its NetworkAddress
+		dataStore.myAgentIDTriple.setAddr(this.network.getAddress());
 	}
 
 	public double getCommunicationRange() {
-		return state.communicationRange;
+		return environmentState.communicationRange;
 	}
 
 	public double getPerceptionRange() {
-		return state.perceptionRange;
+		return environmentState.perceptionRange;
 	}
 
 	public Location getLocation() {
-		return state.loc;
+		return environmentState.loc;
 	}
 
 	public void setLocation(Location l) {
-		this.state.loc = l;
+		this.environmentState.loc = l;
 	}
 
 	@Override
 	protected void processInput(Input in) {
 		if(in instanceof HelloMessage) {
-			HelloMessage m = (HelloMessage) in;
-			logger.info(m.getFrom().getId() +" sent me a HelloMessage, how nice!");
+			handleHelloMessage((HelloMessage)in);
+		}
+		else if (in instanceof AgentInfoMessage) {
+			handleAgentInfoMessage((AgentInfoMessage)in);
 		}
 	}
 
 	@Override
 	protected Set<ParticipantSharedState<?>> getSharedState() {
 		Set<ParticipantSharedState<?>> ss = super.getSharedState();
-		// shared state for ParticipantLocationService
+		// shared environmentState for ParticipantLocationService
 		ss.add(ParticipantLocationService.createSharedState(this.getID(), this));
-		// shared state for network communication range
+		// shared environmentState for network communication range
 		ss.add(CommunicationRangeService.createSharedState(getID(), this));
 		
 		return ss;
@@ -99,15 +125,22 @@ public class HelloAgent extends AbstractParticipant implements HasLocation, HasP
 		super.execute();
 		
 		logger.info("My location is: "+ this.getLocation());
+		//logger.info("My IDs are : " + this.dataStore.myAgentIDTriple);
 		
-		// observe nearby agents
-		for(Map.Entry<UUID, Location> agent : this.locationService.getNearbyAgents().entrySet()) {
-			logger.info("I see agent: "+agent.getKey()+" at location: "+agent.getValue());
-		}
+		observeNearbyAgents();
 		
-		// broadcast hello!
-		this.network.sendMessage(new HelloMessage(this.network.getAddress(), getTime()));
+		broadcastHello();
 		
+		outputKnownAgents();
+		
+		doMove();
+		
+	}
+	
+	/**
+	 * Attempt to make a move action. Will be random.
+	 */
+	protected void doMove(){
 		// random movement
 		Move2D<Integer> move = new Move2D<Integer>(Random.randomInt(10)-5, Random.randomInt(10)-5);
 		
@@ -119,5 +152,92 @@ public class HelloAgent extends AbstractParticipant implements HasLocation, HasP
 			logger.warn(e);
 		}
 	}
+	
+	/**
+	 * Send a broadcasted HelloMessage to all agents in communication range
+	 */
+	protected void broadcastHello(){
+		this.network.sendMessage(new HelloMessage(this.network.getAddress(), getTime()));
+	}
 
+	/**
+	 * Send a message to an agent asking for their UUID and human-readable name
+	 * @param targetAddress NetworkAddress of the target agent
+	 */
+	private void sendAgentInfoMsgRequest(NetworkAddress targetAddress) {
+		this.network.sendMessage(new AgentInfoMessage(Performative.REQUEST, this.network.getAddress(), targetAddress, getTime(), dataStore.myAgentIDTriple ));
+	}
+	
+	/**
+	 * Send a response to a request for agent info
+	 * @param targetAddress
+	 */
+	private void sendAgentInfoMsgInform(NetworkAddress targetAddress){
+		this.network.sendMessage(new AgentInfoMessage(Performative.INFORM, this.network.getAddress(), targetAddress, getTime(), dataStore.myAgentIDTriple ));
+	}
+	
+	/**
+	 * Log the presence of nearby agents
+	 */
+	protected void observeNearbyAgents() {
+		for(Map.Entry<UUID, Location> agent : this.locationService.getNearbyAgents().entrySet()) {
+			logger.info("I see agent: "+agent.getKey()+" at location: "+agent.getValue());
+		}
+	}
+	
+	protected void outputKnownAgents() {
+		logger.info("I know the following agents:");
+		for (Entry<UUID, AgentIDTriple> entry : this.dataStore.knownAgents.entrySet() ) {
+			logger.info("\t " + entry.getValue().toString());
+		}
+	}
+
+	protected void handleHelloMessage(HelloMessage msg) {
+		NetworkAddress originator = msg.getFrom();
+		String string = originator.getId() + " sent me a HelloMessage, ";
+		if (dataStore.knownAgents.containsKey(originator.getId())) {
+			string = string + "and I already know them.";
+		}
+		else {
+			string = string + "and I haven't met them before, so I'll add them to my list and ask them for more info.";
+			dataStore.knownAgents.put(originator.getId(), new AgentIDTriple(null, null, originator));
+			sendAgentInfoMsgRequest(originator);
+		}
+		logger.info(string);
+	}
+
+	private void handleAgentInfoMessage(AgentInfoMessage msg) {
+		Performative perf = msg.getPerformative();
+		NetworkAddress originator = msg.getFrom();
+		if (perf.equals(Performative.REQUEST)) {
+			logger.info(originator.getId() + " sent me a request for AgentInfo, so I will add them to my list and send my info back.");
+			sendAgentInfoMsgInform(originator);
+			addAgentInfo(msg.getContent());
+		}
+		else if (perf.equals(Performative.INFORM)) {
+			logger.info(originator.getId() + " replied to my request for AgentInfo, so I will add them to my list.");
+			addAgentInfo(msg.getContent());
+		}
+	}
+	
+	private void addAgentInfo(AgentIDTriple info){
+		if (!this.dataStore.knownAgents.containsKey(info.getUuid())) {
+			dataStore.knownAgents.put(info.getUuid(), new AgentIDTriple(info.getUuid(), info.getName(), info.getAddr()));
+		}
+		else {
+			logger.info("Current info: " + dataStore.knownAgents.get(info.getUuid()));
+			dataStore.knownAgents.get(info.getUuid()).update(info.getUuid(), info.getName(), info.getAddr());
+			logger.info("Updated info: " + dataStore.knownAgents.get(info.getUuid()));
+		}
+	}
+	
+	private void addAgentInfo(UUID uuid, String name, NetworkAddress addr) {
+		if (!this.dataStore.knownAgents.containsKey(uuid)) {
+			dataStore.knownAgents.put(uuid, new AgentIDTriple(uuid, name, addr));
+		}
+		else {
+			dataStore.knownAgents.get(uuid).update(uuid, name, addr);
+		}
+	}
+	
 }
